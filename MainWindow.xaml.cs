de643fbedc3f39edbe3753492287d4d0e7bc5583
NataliaSidz <92212990+NataliaSidz@users.noreply.github.com>
2022-11-15 14:29:59 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Order order;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            order = new Order();
+        }
+
+        private void DisplayFinalSummary()
+        {
+            TextBox_Summary.Text = order.ToString();
+        }
+
+        #region Buttons
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result;
+            result = MessageBox.Show("Zamówienie zostało przyjęte", "Zrobione", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.Yes);
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        #region Checkboxes
+        private void ColorPaper_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool isChecked = ((CheckBox)sender).IsChecked ?? false;
+            ComboBox_PaperColors.IsEnabled = isChecked;
+            order.BonusOptionsMultiplier += isChecked ? 0.5 : -0.5;
+            DisplayFinalSummary();
+        }
+
+        private void BonusOptions_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            order.BonusOptionsMultiplier += cb.IsChecked == true ? double.Parse(cb.Tag.ToString()) : -double.Parse(cb.Tag.ToString());
+            DisplayFinalSummary();
+        }
+        #endregion
+
+        #region RadioButtons
+        private void Grammage_Change(object sender, RoutedEventArgs e)
+        {
+            switch (((RadioButton)sender).Tag)
+            {
+                case "80":
+                    order.GrammageChangeMultiplier = 1;
+                    break;
+                case "120":
+                    order.GrammageChangeMultiplier = 2;
+                    break;
+                case "200":
+                    order.GrammageChangeMultiplier = 2.5;
+                    break;
+                case "240":
+                    order.GrammageChangeMultiplier = 3;
+                    break;
+            }
+            order.Grammary = ((RadioButton)sender).Tag + " g/m²";
+            DisplayFinalSummary();
+        }
+
+        private void LeadTime_Change(object sender, RoutedEventArgs e)
+        {
+            order.LeadTimeAddedCost = int.Parse(((RadioButton)sender).Tag.ToString());
+            order.LeadTime = ((RadioButton)sender).Tag.ToString() == "0" ? "Standard": "Ekspresowy";
+            DisplayFinalSummary();
+        }
+        #endregion
+
+        private void PaperColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cmb = (ComboBox)sender;
+            order.PaperColor = cmb.SelectedItem.ToString();
+            DisplayFinalSummary();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            order.BasePrice = 0.2;
+            for (int i = 0; i < Math.Round(((Slider)sender).Value); i++)
+                order.BasePrice *= 2.5;
+            Label_Format.Content = "A" + (5 - Math.Round(((Slider)sender).Value)) + " - cena " + order.BasePrice * 100 + "gr/szt.";
+            DisplayFinalSummary();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!string.IsNullOrEmpty(textBox.Text))
+            {
+                order.Count = int.Parse(textBox.Text);
+                DisplayFinalSummary();
+            }
+            else
+            {
+                TextBox_Summary.Text = "";
+            }
+        }
+
+        private void ValidateText(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zadanie2
+{
+    internal class Order
+    {
+        public int Count { get; set; } = 0;
+        public string Format { get; set; } = "A5";
+        public string Grammary { get; set; } = "80 g/m²";
+        public string PrintType { get; set; } = "Normalny";
+        public string PaperColor { get; set; } = "Biały";
+        public string LeadTime { get; set; } = "Standard";
+        public double Discount
+        {
+            get
+            {
+                return 1 - Math.Floor((double)Count / 100) / 100;
+            }
+            private set { }
+        }
+        public double FinalPrice
+        {
+            get
+            {
+                return Count * BasePrice * Discount * GrammageChangeMultiplier * BonusOptionsMultiplier + LeadTimeAddedCost;
+            }
+            private set { }
+        }
+        public double BasePrice { get; set; } = 0.2;
+        public double GrammageChangeMultiplier { get; set; } = 1;
+        public double BonusOptionsMultiplier { get; set; } = 1;
+        public int LeadTimeAddedCost { get; set; } = 0;
+
+        public override string ToString()
+        {
+            return "Przedmiot zamówienia: " + Count + "szt." + ", Format: " + Format + ", Gramatura: " + Grammary + "\n"
+                + "Czas realizacji: " + LeadTime + ", Papier: " + PaperColor + "\n"
+                + "Cena przed rabatem: " + BasePrice + "zł\n"
+                + "Naliczony rabat: " + Math.Floor((1 - Discount) * 100) + "%\n"
+                + "Cena po rabacie: " + Math.Round(FinalPrice, 2).ToString("0.00") + "zł";
+        }
+    }
+}
